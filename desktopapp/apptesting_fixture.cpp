5946b84e4e152d1fe0ec2980db5ada36689627dc
Tarcisio Fischer <tarcisio.fischer@codethink.co.uk>
2024-03-12 12:52:00 +0000

    Cleanup TestMainWindow, trying to make sure virtual members can be seen
    I don't know why suse_tumbleweed_qt66 CI is _sometimes_ giving a
    compilation error saying 'undefined reference to `vtable for TestMainWindow'.
    
    This commit changes the TestMainWindow, trying to make sure that the
    compiler has access(?) to all virtual members. It is more an "empirical"
    solution than an actual one, but it improves code readability IMO anyway.
    
    Signed-off-by: Tarcisio Fischer <tarcisio.fischer@codethink.co.uk>

--- desktopapp/apptesting_fixture.cpp
+++ desktopapp/apptesting_fixture.cpp
@@ -35,7 +35,7 @@ CodeVisApplicationTestFixture::CodeVisApplicationTestFixture()
 
     // Resources must be initialized before mainwindow, so I can't initialize this on
     // the initialization list.
-    mainWindow = new TestMainWindow(sharedNodeStorage, &undoManager);
+    mainWindow = std::make_unique<TestMainWindow>(sharedNodeStorage, &undoManager);
     mainWindow->show();
 }
 
@@ -209,19 +209,22 @@ void CodeVisApplicationTestFixture::clickOn(ClickableFeature const& feature)
 
 void CodeVisApplicationTestFixture::ctrlZ()
 {
-    QTest::keySequence(mainWindow, static_cast<QKeySequence>(static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::Key_Z)));
+    QTest::keySequence(mainWindow.get(),
+                       static_cast<QKeySequence>(static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::Key_Z)));
     QTest::qWait(100);
 }
 
 void CodeVisApplicationTestFixture::ctrlShiftZ()
 {
+    auto sequence =
 #if defined(Q_OS_WINDOWS)
-    QTest::keySequence(mainWindow, static_cast<QKeySequence>(static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::Key_Y)));
+        static_cast<QKeySequence>(static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::Key_Y))
 #else
-    QTest::keySequence(mainWindow,
-                       static_cast<QKeySequence>(static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::SHIFT)
-                                                 | static_cast<int>(Qt::Key_Z)));
+        static_cast<QKeySequence>(static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::SHIFT)
+                                  | static_cast<int>(Qt::Key_Z))
 #endif
+        ;
+    QTest::keySequence(mainWindow.get(), sequence);
     QTest::qWait(100);
 }
 
@@ -253,4 +256,9 @@ QPoint CodeVisApplicationTestFixture::findElementTopLeftPosition(const std::stri
     const auto offset = QPointF(10, 10);
     const auto p = entity->scenePos() + offset;
     return view->mapFromScene(p);
+}
+
+TestMainWindow& CodeVisApplicationTestFixture::window()
+{
+    return *mainWindow;
 }
